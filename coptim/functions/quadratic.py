ae98b20fb1eb96f83ba8f549b9b4724dd6cfba48
Cody Mazza-Anthony <cmazzaanthony@gmail.com>
2019-05-02 20:37:26 -0400

    Added more details to quad function

--- coptim/functions/quadratic.py
+++ coptim/functions/quadratic.py
@@ -6,3 +6,9 @@ class Quadratic(Function):
 
     def eval(self, Q, c, x, gamma):
         return 0.5 * x.T.dot(Q).dot(x) + c + gamma
+
+    def gradient(self, Q, c, x):
+        return Q.dot(x) + c
+
+    def hessian(self):
+        pass

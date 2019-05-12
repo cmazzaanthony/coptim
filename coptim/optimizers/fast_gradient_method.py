a68ab9430d11cd777af4ca3dbb91ac78c884300f
Cody Mazza-Anthony <cmazzaanthony@gmail.com>
2019-05-12 10:10:08 -0400

    Test passes for inexact method, moving to fast gradient method

--- coptim/optimizers/fast_gradient_method.py
+++ coptim/optimizers/fast_gradient_method.py
@@ -7,10 +7,29 @@ class FastGradientMethod(Optimizer):
         self.iterations = 0
 
     def optimize(self, x_0, func, beta, sigma, epsilon):
-        pass
+        Q = np.array([[1, 0, 0, 0],
+                      [0, 1, 0, 0],
+                      [0, 0, 1, 0],
+                      [0, 0, 0, delta]])
+        L = np.linalg.norm(Q)
+        c = np.array([1, 1, 1, 1])
+        x = x_0
+        y = x_0
+        alpha = 1
+        k = 0
+        while np.linalg.norm(dfunct(Q, c, x)) >= epsilon:
+            prev_x = x
+            x = y - 1 / L * dfunct(Q, c, y)
+
+            prev_alpha = alpha
+            alpha = (1 + np.sqrt(1 + 4 * np.power(prev_alpha, 2))) / 2
+
+            y = x + (prev_alpha - 1) / alpha * (x - prev_x)
+
+            k += 1
 
     def stopping_criteria(self, x, func, epsilon):
-        pass
+        return np.linalg.norm(dfunct(Q, c, x)) >= epsilon
 
     def step_size(self, x, func, beta, d, sigma):
         pass
--- tests/optimizers/test_gradient_method_exact_minimization.py
+++ tests/optimizers/test_gradient_method_exact_minimization.py
@@ -13,9 +13,23 @@ class TestGradientMethodExactMinimization(unittest.TestCase):
         starting_point = np.array([1, 2, 3, 4])
         deltas = np.array([0.1, 0.01, 0.001, 0.0001, 0.00001, 0.000001])
         epsilon = 0.00001
+        estimates = list()
+        iterations = list()
         for delta in deltas:
             optimizer = GradientMethodExactMinimization()
             x = optimizer.optimize(starting_point,
                                    objective,
                                    delta,
                                    epsilon)
+
+            estimates.append(x)
+            iterations.append(optimizer.iterations)
+
+        self.assertListEqual(iterations, [
+            23,
+            91,
+            761,
+            7449,
+            74321,
+            743055
+        ])

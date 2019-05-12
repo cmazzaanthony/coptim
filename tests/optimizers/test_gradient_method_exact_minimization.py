e22173ba1c1fe3dbe246383b24351f656e32063d
Cody Mazza-Anthony <cmazzaanthony@gmail.com>
2019-05-11 20:01:13 -0400

    Added more details

--- tests/optimizers/test_gradient_method_exact_minimization.py
+++ tests/optimizers/test_gradient_method_exact_minimization.py
@@ -9,24 +9,26 @@ from coptim.optimizers.gradient_method_exact_minimization import GradientMethodE
 class TestGradientMethodExactMinimization(unittest.TestCase):
 
     def test_inexact_gradient_method(self):
+        objective = Brown()
+        starting_point = np.array([1, 2, 3, 4])
         deltas = np.array([0.1, 0.01, 0.001, 0.0001, 0.00001, 0.000001])
+        epsilon = 0.00001
         for delta in deltas:
-            gradient_method(x_0=np.array([1, 2, 3, 4]),
-                            func=Brown(),
-                            delta=delta,
-                            epsilon=0.00001)
+            optimizer.optimize(starting_point,
+                               objective,
+                               delta,
+                               epsilon)
 
-            objective = Brown()
+
             starting_point = np.array([-1.2, 1])
             beta = 0.5
             sigma = 0.0001
             epsilon = 0.0001
 
-            optimizer = GradientMethodExactMini/c/mization()
+            optimizer = GradientMethodExactMinimization()
 
             x = optimizer.optimize(starting_point,
                                    objective,
                                    beta,
                                    sigma,
                                    epsilon)
-

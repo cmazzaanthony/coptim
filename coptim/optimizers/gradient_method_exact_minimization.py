583d8be35edf32487088070e646e174a15b038c4
Cody Mazza-Anthony <cmazzaanthony@gmail.com>
2019-05-12 10:05:00 -0400

    More work on unit test for inexact method

--- coptim/optimizers/gradient_method_exact_minimization.py
+++ coptim/optimizers/gradient_method_exact_minimization.py
@@ -8,21 +8,21 @@ class GradientMethodExactMinimization(Optimizer):
         # TODO: More metrics: vector of x's, objective values, etc.
         self.iterations = 0
 
-    def step_size(self, Q, c, x, d):
-        g = self.dfunct(Q, c, x)
+    def step_size(self, Q, c, x, d, func):
+        g = func.gradient(Q, c, x)
         return -1 * g.T.dot(d) / d.T.dot(Q).dot(d)
 
-    def optimize(self, x_0, delta, epsilon):
+    def optimize(self, x_0, func, delta, epsilon):
         Q = np.array([[1, 0, 0, 0],
                       [0, 1, 0, 0],
                       [0, 0, 1, 0],
                       [0, 0, 0, delta]])
         c = np.array([1, 1, 1, 1])
         x = x_0
-        while self.stopping_criteria(x, Q, c, epsilon):
-            descent_direction = -1 * self.dfunct(Q, c, x)
+        while self.stopping_criteria(x, Q, c, func, epsilon):
+            descent_direction = -1 * func.gradient(Q, c, x)
 
-            step_size = self.step_size(Q, c, x, descent_direction)
+            step_size = self.step_size(Q, c, x, descent_direction, func)
 
             # update step
             x = x + step_size * descent_direction
@@ -31,5 +31,5 @@ class GradientMethodExactMinimization(Optimizer):
 
         return x
 
-    def stopping_criteria(self, x, Q, c, epsilon):
-        return np.linalg.norm(self.dfunct(Q, c, x)) >= epsilon
+    def stopping_criteria(self, x, Q, c, func, epsilon):
+        return np.linalg.norm(func.gradient(Q, c, x)) >= epsilon
--- tests/optimizers/test_gradient_method_exact_minimization.py
+++ tests/optimizers/test_gradient_method_exact_minimization.py
@@ -2,33 +2,20 @@ import unittest
 
 import numpy as np
 
-from coptim.functions.brown import Brown
+from coptim.functions.quadratic import Quadratic
 from coptim.optimizers.gradient_method_exact_minimization import GradientMethodExactMinimization
 
 
 class TestGradientMethodExactMinimization(unittest.TestCase):
 
     def test_inexact_gradient_method(self):
-        objective = Brown()
+        objective = Quadratic()
         starting_point = np.array([1, 2, 3, 4])
         deltas = np.array([0.1, 0.01, 0.001, 0.0001, 0.00001, 0.000001])
         epsilon = 0.00001
         for delta in deltas:
-            optimizer.optimize(starting_point,
-                               objective,
-                               delta,
-                               epsilon)
-
-
-            starting_point = np.array([-1.2, 1])
-            beta = 0.5
-            sigma = 0.0001
-            epsilon = 0.0001
-
             optimizer = GradientMethodExactMinimization()
-
             x = optimizer.optimize(starting_point,
                                    objective,
-                                   beta,
-                                   sigma,
+                                   delta,
                                    epsilon)

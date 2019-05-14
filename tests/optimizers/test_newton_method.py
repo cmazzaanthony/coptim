e702e15bf53de4fc9fc292ffbbaaf26a2ccb5d56
Cody Mazza-Anthony <cmazzaanthony@gmail.com>
2019-05-13 21:37:10 -0400

    Added test case that fails, need to make it run now

--- tests/optimizers/test_newton_method.py
+++ tests/optimizers/test_newton_method.py
@@ -2,8 +2,8 @@ import unittest
 
 import numpy as np
 
+from coptim.functions.brown import Brown
 from coptim.functions.rosenbrock import Rosenbrock
-from coptim.optimizers.gradient_method import GradientMethod
 from coptim.optimizers.newton_method import NewtonMethod
 
 
@@ -21,10 +21,36 @@ class TestNewtonMethod(unittest.TestCase):
 
         optimizer = NewtonMethod()
 
-        x = optimizer.optimize(x_0=starting_point,
-                               rho=rho,
-                               p=p,
-                               k_max=k_max,
-                               beta=beta,
-                               sigma=sigma,
-                               epsilon=epsilon)
+        optimizer.optimize(x_0=starting_point,
+                           rho=rho,
+                           p=p,
+                           k_max=k_max,
+                           beta=beta,
+                           sigma=sigma,
+                           epsilon=epsilon,
+                           func=objective)
+
+        self.assertEqual(optimizer.iterations, 8)
+
+    def test_newton_method_with_brown_objective(self):
+        starting_point = np.array([1.2, 1])
+        rho = 1e-8
+        p = 2.1
+        k_max = 200
+        beta = 0.5
+        sigma = 1e-4
+        epsilon = 1e-6
+        objective = Brown()
+
+        optimizer = NewtonMethod()
+
+        optimizer.optimize(x_0=starting_point,
+                           rho=rho,
+                           p=p,
+                           k_max=k_max,
+                           beta=beta,
+                           sigma=sigma,
+                           epsilon=epsilon,
+                           func=objective)
+
+        self.assertEqual(optimizer.iterations, 8)

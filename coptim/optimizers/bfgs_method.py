89b046e6e325d0b171ca0c29651a35d6a8cbf56b
Cody Mazza-Anthony <cmazzaanthony@gmail.com>
2019-05-15 21:03:55 -0400

    more cleanup of bfgs method

--- coptim/optimizers/bfgs_method.py
+++ coptim/optimizers/bfgs_method.py
@@ -8,10 +8,10 @@ class BFGSMethod(Optimizer):
         # TODO: More metrics: vector of x's, objective values, etc.
         self.iterations = 0
 
-    def psi(self, t, sigma, x, d, func, gradient):
-        return func(x + t * d) - \
-               func(x) - \
-               sigma * t * gradient(x).dot(d)
+    def psi(self, t, sigma, x, d, func):
+        return func.eval(x + t * d) - \
+               func.eval(x) - \
+               sigma * t * func.gradient(x).dot(d)
 
     def wolfe_powell_rule_phase_B(self, a, b, rho, sigma, x, d, func):
         a_j = a
@@ -21,16 +21,16 @@ class BFGSMethod(Optimizer):
         while not stop:
             t_j = a_j + (b_j - a_j) / 2
 
-            if self.psi(t_j, sigma, x, d, func, gradient) >= 0:
+            if self.psi(t_j, sigma, x, d, func) >= 0:
                 b_j = t_j
 
-            if self.psi(t_j, sigma, x, d, func, gradient) < 0 and \
-                    gradient(x + t_j * d).dot(d) >= rho * gradient(x).dot(d):
+            if self.psi(t_j, sigma, x, d, func) < 0 and \
+                    func.gradient(x + t_j * d).dot(d) >= rho * func.gradient(x).dot(d):
                 t = t_j
                 stop = True
 
-            if self.psi(t_j, sigma, x, d, func, gradient) < 0 and \
-                    gradient(x + t_j * d).dot(d) < rho * gradient(x).dot(d):
+            if self.psi(t_j, sigma, x, d, func) < 0 and \
+                    func.gradient(x + t_j * d).dot(d) < rho * func.gradient(x).dot(d):
                 a_j = t_j
 
         return t
@@ -48,61 +48,53 @@ class BFGSMethod(Optimizer):
 
                 t_i = self.wolfe_powell_rule_phase_B(a, b, rho, sigma, x, d, func)
 
-            if self.psi(t_i, sigma, x, d, func, gradient) < 0 and \
-                    gradient(x + t_i * d).dot(d) >= rho * gradient(x).dot(d):
+            if self.psi(t_i, sigma, x, d, func) < 0 and \
+                    func.gradient(x + t_i * d).dot(d) >= rho * func.gradient(x).dot(d):
                 t = t_i
                 stop = True
 
-            if self.psi(t_i, sigma, x, d, func, gradient) < 0 and \
-                    gradient(x + t_i * d).dot(d) < rho * gradient(x).dot(d):
+            if self.psi(t_i, sigma, x, d, func) < 0 and \
+                    func.gradient(x + t_i * d).dot(d) < rho * func.gradient(x).dot(d):
                 i += 1
                 t_i = gamma * t_i
 
         return t
 
-    def optimize(x_0, H_0, rho, sigma, epsilon, func, gradient):
+    def optimize(self, x_0, H_0, rho, sigma, epsilon, func, gradient):
         x = x_0
         H = H_0
-        k = 0
         while np.linalg.norm(gradient(x)) > epsilon:
             nabla_x = -gradient(x)
 
             inverse = np.linalg.inv(H)
             d = inverse.dot(nabla_x)
 
-            step_size = wolfe_powell_rule_phase_A(rho=rho,
-                                                  sigma=sigma,
-                                                  x=x,
-                                                  d=d,
-                                                  func=func,
-                                                  gradient=gradient)
+            step_size = self.step_size(rho=rho,
+                                       sigma=sigma,
+                                       x=x,
+                                       d=d,
+                                       func=func)
 
-            # print("step size: {}".format(step_size))
-            # print("gradient {}".format(nabla_x))
             x_new = x + step_size * d
             s = x_new - x
             y = gradient(x_new) - gradient(x)
 
             H = H + (np.outer(y, y.T) / np.dot(y.T, s)) - np.outer(H.dot(s), s.T).dot(H) / (np.dot(np.dot(s.T, H), s))
 
-            # print("new H: {}".format(H))
-            # print("new x: {}".format(x_new))
             x = x_new
-            k += 1
+            self.iterations += 1
 
-        print("final x: {}".format(x_new))
-        print("iterations needed {}".format(k))
-        return k
+        return x
 
-
-def stopping_criteria(self, x, func, epsilon):
+    def stopping_criteria(self, x, func, epsilon):
         return np.linalg.norm(func.gradient(x)) >= epsilon
 
     def step_size(self, x, func, beta, d, sigma):
         i = 0
         inequality_satisfied = True
         while inequality_satisfied:
-            if func.eval(x + np.power(beta, i) * d) <= func.eval(x) + np.power(beta, i) * sigma * func.gradient(x).dot(d):
+            if func.eval(x + np.power(beta, i) * d) <= func.eval(x) + np.power(beta, i) * sigma * func.gradient(x).dot(
+                    d):
                 break
 
             i += 1

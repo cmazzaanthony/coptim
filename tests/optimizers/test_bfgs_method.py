10fd8f659489f27b79fd226de8b29b00d764786f
Cody Mazza-Anthony <cmazzaanthony@gmail.com>
2019-05-15 21:12:34 -0400

    Started work on second unit test with bateman function

--- tests/optimizers/test_bfgs_method.py
+++ tests/optimizers/test_bfgs_method.py
@@ -27,3 +27,23 @@ class TestBFGSMethod(unittest.TestCase):
                                objective)
 
         self.assertEqual(optimizer.iterations, 34)
+
+    def test_bfgs_method_with_brown_objective(self):
+        objective = Bateman()
+        starting_point = np.array([0.05, 0.1, 0.4])
+        H_0 = np.array([[1, 0, 0],
+                        [0, 1, 0],
+                        [0, 0, 1]])
+        rho = 0.9
+        sigma = 1e-4
+        epsilon = 1e-6
+
+        optimizer = BFGSMethod()
+        x = optimizer.optimize(starting_point,
+                               H_0,
+                               rho,
+                               sigma,
+                               epsilon,
+                               objective)
+
+        self.assertEqual(optimizer.iterations, 34)

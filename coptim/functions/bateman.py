235ac741d0431cc3a50f02a1759b736411c48d50
Cody Mazza-Anthony <cmazzaanthony@gmail.com>
2019-05-18 10:12:56 -0400

    Added bateman function

---
+++ coptim/functions/bateman.py
@@ -0,0 +1,14 @@
+import numpy as np
+from coptim.function import Function
+
+
+class Bateman(Function):
+
+    def eval(self, x):
+        pass
+
+    def gradient(self, x):
+        pass
+
+    def hessian(self, x):
+        pass

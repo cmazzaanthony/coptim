38995423666484e82116c8478e8e6f20083cdeb2
Cody Mazza-Anthony <cmazzaanthony@gmail.com>
2019-05-28 20:50:25 -0400

    Added defaults for class

--- coptim/functions/bateman.py
+++ coptim/functions/bateman.py
@@ -1,10 +1,13 @@
 import numpy as np
 from coptim.function import Function
 
+T = np.array([15, 25, 35, 45, 55, 65, 75, 85, 105, 185, 245, 305, 365])
+Y = np.array([0.038, 0.085, 0.1, 0.103, 0.093, 0.095, 0.088, 0.08, 0.073, 0.05, 0.038, 0.028, 0.02])
+
 
 class Bateman(Function):
 
-    def eval(self, x):
+    def eval(self, x, t_list=T):
         y_x_t = x[2] * (np.exp(-x[0] * t_list) - np.exp(-x[1] * t_list))
         f_raw = np.power(y_x_t - Y, 2)
 
@@ -22,7 +25,6 @@ class Bateman(Function):
             dx3 = dx3 + (np.exp(-x[0] * T[i]) - np.exp(-x[1] * T[i])) * (y_x_t[i] - Y[i])
 
         return np.array([dx1, dx2, dx3])
-
 
     def hessian(self, x):
         pass
